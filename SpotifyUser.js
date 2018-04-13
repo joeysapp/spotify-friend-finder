74c8385e8a8fb139280e4a86684469f569638548
Joseph Sapp <sapp.joey@tamu.edu>
2018-04-12 23:06:39 -0500

    good enough for now

---
+++ SpotifyUser.js
@@ -0,0 +1,11 @@
+class SpotifyUser extends React.Component {
+	constructor(username, uuid){
+		this.username = username;
+		this.uuid = uuid;
+		this.favorite_artists = new Array(50);
+		this.playlists = new Array(50);
+	}
+	render() {
+		return <h1>hey y'all website be down</h1>
+	}
+}
