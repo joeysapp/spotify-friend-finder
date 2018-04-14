ffd3e92a6b03fad03c2538d9a6a0b3a263d6fc26
Joseph Sapp <sapp.joey@tamu.edu>
2018-04-14 13:51:21 -0500

    conditional assignments are hard

--- public/bundle-main.js
+++ public/bundle-main.js
@@ -45172,7 +45172,7 @@ class SpotifyUser extends React.Component {
 	render() {
 		var now_playing;
 		if (typeof this.user.now_playing.item !== 'undefined'){
-			now_playing = '' || this.user.now_playing.item.name;
+			now_playing = this.user.now_playing.item.name;
 
 		}
 
