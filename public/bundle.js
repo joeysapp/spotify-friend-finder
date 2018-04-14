febbbbcf2328fa5b3afd5da2f1e96717d77d7ebe
Joseph Sapp <sapp.joey@tamu.edu>
2018-04-14 13:44:59 -0500

    does this work?

--- public/bundle.js
+++ public/bundle.js
@@ -86,10 +86,8 @@ class SpotifyUser extends React.Component {
 
 	render() {
 
-		var now_playing = '';
-		if (this.user.now_playing){
-			now_playing = this.user.now_playing.item.name;
-		}
+
+		var now_playing = '' || this.user.now_playing.item.name;
 
 		return (
 			<div className='spotifyUser'>
